a85361964c54da66ee86ff373ae281cf89eaecd8
Margarita Sukhinina <svk06smv85@yandex.ru>
2024-09-20 12:40:50 +1000

    Mestro-progect

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/MargaritaSukhinina/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+const cardItem = cardTemplate.querySelector('.card');
+
 // @todo: DOM узлы
+const cardList = document.querySelector('.places__list');
+
 
 // @todo: Функция создания карточки
+function renderCard() {
+    initialCards.forEach(function(element) {
+        const card = cardItem.cloneNode(true);
+        
+        card.querySelector('.card__image').src = element.link;
+        card.querySelector('.card__image').alt = element.name;
+        card.querySelector('.card__title').textContent = element.name;
+
+        cardList.append(card);
+
+        deletCard(card)
+    })
+}
 
 // @todo: Функция удаления карточки
+function deletCard(card) {
+    
+    const cardDeleteButton = card.querySelector('.card__delete-button');
+    
+    cardDeleteButton.addEventListener('click', function() {
+       card.remove();  
+    });
+}   
 
 // @todo: Вывести карточки на страницу
+renderCard()
